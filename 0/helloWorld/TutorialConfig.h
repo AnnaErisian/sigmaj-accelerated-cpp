25e10a31f9229e73a498cbd212ad26ec2f58a4ca
Jacob Inkrote <jacob.inkrote@gmail.com>
2017-10-10 20:51:06 -0400

    Hello World and starting with CMake

---
+++ 0/helloWorld/TutorialConfig.h
@@ -0,0 +1,3 @@
+//Cmake configures options/settings
+#define VERSION_NUMBER_MAJOR 0
+#define VERSION_NUMBER_MINOR 1
---
+++ 0/helloWorld/hw.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+int main() {
+    std::cout << "Hello cpp" << std::endl;
+    std::cout << VERSION_NUMBER_MAJOR << "." << VERSION_NUMBER_MINOR << std::endl;
+}
