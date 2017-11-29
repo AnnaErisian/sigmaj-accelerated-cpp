67721ee954d7f37151220973440db19598635432
Jacob Inkrote <jacob.inkrote@gmail.com>
2017-11-28 21:31:20 -0500

    Chapter 8 library reimplementation search + equal
    Search: I was returning the wrong iterator - the internal loop begin,
    which pointed to the end, rather than the external begin, which interal
    begin copies from before checking the full sequence
    
    Equal: I was failing to ensure that there were no additional elements in
    the first sequence

--- 8/functions.h
+++ 8/functions.h
@@ -1,13 +1,17 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <iostream>
+
 template <class In>
 bool equal(In b, In e, In b2) {
   while(b != e) {
-    if(*b != *e) return false;
+    if(*b != *b2) return false;
     ++b;
+    ++b2;
   }
-  return true;
+  //if both are one-off-the-end, we are equal
+  return *b == *b2;
 }
 
 template <class In, class X>
@@ -28,13 +32,118 @@ In copy(In b, In e, Out d) {
   return d;
 }
 
-remove_copy_if(b, e, d, p)
-transform(b, e, d, f)
-accumulate(b, e, t)
-search(b, e, b2, e2)
-find_if(b, e, p)
-remove_copy(b, e, d, t)
-remove(b, e, t)
-partition(b, e, p)
+template <class For, class Out, class Pred>
+For remove_copy_if(For b, For e, Out d, Pred p) {
+  For b2 = b;
+  while(b2 != e) {
+    if(p(*b2)) {
+      //output match
+      d = *b2;
+      d++;
+      
+      //move match to front
+      swap(b, b2);
+      b++;
+      b2++;
+    } else {
+      b2++;
+    }
+    //Invariants
+    //all elements before b are matches: [0, b)
+    //all elements between b and b2 are not: [b, b2)
+  }
+  return b;
+}
+
+
+template <class For, class Out, class T>
+For remove_copy(For b, For e, Out d, T t) {
+  For b2 = b;
+  while(b2 != e) {
+    if(*b2 == t) {
+      //output match
+      d = *b2;
+      d++;
+      
+      //move match to front
+      swap(b, b2);
+      b++;
+      b2++;
+    } else {
+      b2++;
+    }
+    //Invariants
+    //all elements before b are matches: [0, b)
+    //all elements between b and b2 are not: [b, b2)
+  }
+  return b;
+}
+
+template <class In, class Out, class F>
+Out transform(In b, In e, Out d, F f) {
+  while(b != e) {
+    d = f(*b);
+    ++b;
+    ++d;
+  }
+  return d;
+}
+
+template <class In, class T>
+T accumulate(In b, In e, T t) {
+  T x;
+  while(b != e) x += *b;
+  return x;
+}
+
+template <class In>
+In search(In b, In e, In b2, In e2) {
+  while(b != e) {
+    if(*b == *b2) {
+      In sb = b;
+      In sb2 = b2;
+      while(*sb == *sb2) {
+        sb++;
+        sb2++;
+        if(sb2 == e2) return b;
+      }
+    }
+    b++;
+  }
+  return e;
+}
+
+template <class In, class Pred>
+In find_if(In b, In e, Pred p) {
+  while(b != e) {
+    if(p(*b)) break;
+    b++;
+  }
+  return b;
+}
+
+template <class For, class T>
+For remove(For b, For e, T t) {
+  For b2 = b;
+  while(b2 != e) {
+    if(*b2 == t) {
+      //move match to front
+      swap(b, b2);
+      b++;
+      b2++;
+    } else {
+      b2++;
+    }
+    //Invariants
+    //all elements before b are matches: [0, b)
+    //all elements between b and b2 are not: [b, b2)
+  }
+  return b;
+}
+
+template <class For, class Pred>
+For partition(For b, For e, Pred p) {
+  return remove_if(b,e,p);
+}
 
 #endif
--- 8/harness.cpp
+++ 8/harness.cpp
@@ -1,1 +1,93 @@
- 
+#include "functions.h"
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+static bool test_search() {
+  int a[] = {1, 2, 3, 4, 5, 6, 7};
+  int b[] = {1, 2, 4, 1, 2, 3, 4};
+  int c[] = {1, 2, 3};
+  int d[] = {4, 5};
+  int e[] = {7};
+  
+  vector<int> alpha(a, a+7);
+  vector<int> beta(b, b+7);
+  vector<int> gamma(c, c+3);
+  vector<int> delta(d, d+2);
+  vector<int> epsilon(e, e+1);
+  
+  typedef vector<int>::const_iterator iter;
+  
+  iter r1 = search(alpha.begin(), alpha.end(), beta.begin(),    beta.end());
+  iter r2 = search(alpha.begin(), alpha.end(), gamma.begin(),   gamma.end());
+  iter r3 = search(alpha.begin(), alpha.end(), delta.begin(),   delta.end());
+  iter r4 = search(alpha.begin(), alpha.end(), epsilon.begin(), epsilon.end());
+  iter r5 = search(beta.begin(),  beta.end(),  gamma.begin(),   gamma.end());
+  
+//  if(r1 == alpha.end()) std::cout << "1" << std::endl;
+//  if(r2 == alpha.begin()) std::cout << "2" << std::endl;
+//  if(r3 == alpha.begin() + 3) std::cout << "3" << std::endl;
+//  if(r4 == alpha.end() - 1) std::cout << "4" << std::endl;
+//  if(r5 == beta.begin() + 3) std::cout << "5" << std::endl;
+
+  return  r1 == alpha.end() &&
+          r2 == alpha.begin() &&
+          r3 == alpha.begin() + 3 &&
+          r4 == alpha.end() - 1 &&
+          r5 == beta.begin() + 3;
+}
+
+static bool test_equal() {
+  int a[] = {1, 2, 3, 4, 5, 6, 7};
+  int b[] = {1, 2, 4, 1, 2, 3, 4};
+  int c[] = {1, 2, 3};
+  int d[] = {4, 5};
+  
+  vector<int> alpha(a, a+7);
+  vector<int> beta(b, b+7);
+  vector<int> gamma(c, c+3);
+  vector<int> delta(c, c+3);
+  vector<int> epsilon(d, d+2);
+  
+  //true
+  bool r1 = equal(alpha.begin(), alpha.end(), alpha.begin());
+  bool r2 = equal(gamma.begin(), gamma.end(), delta.begin());
+  
+  //false
+  bool r3 = equal(alpha.begin(), alpha.end(), beta.begin());
+  bool r4 = equal(alpha.begin(), alpha.end(), gamma.begin());
+  bool r5 = equal(alpha.begin(), alpha.end(), epsilon.begin());
+  bool r6 = equal(gamma.begin(), gamma.end(), alpha.begin());
+  
+//  if(r1) std::cout << "1" << std::endl;
+//  if(r2) std::cout << "2" << std::endl;
+//  if(!r3) std::cout << "3" << std::endl;
+//  if(!r4) std::cout << "4" << std::endl;
+//  if(!r5) std::cout << "5" << std::endl;
+//  if(!r6) std::cout << "6" << std::endl;
+    
+  return  r1 && r2 && !r3 && !r4 && !r5 && !r6;
+}
+
+static void run_print_results(std::string name, bool b()) {
+  if(b()) {
+    std::cout << "[O]" << name << " passed!" << std::endl;
+  } else {
+    std::cout << "[ ]" << name << " failed!" << std::endl;
+  }
+}
+
+int main() {
+  run_print_results("test_equal", test_equal);
+  run_print_results("test_search", test_search);
+//  run_print_results("test_find", test_find);
+//  run_print_results("test_find_if", test_find_if);
+//  run_print_results("test_transform", test_transform);
+//  run_print_results("test_accumulate", test_accumulate);
+//  run_print_results("test_copy", test_copy);
+//  run_print_results("test_remove", test_remove);
+//  run_print_results("test_remove_copy", test_remove_copy);
+//  run_print_results("test_remove_copy_if", test_remove_copy_if);
+//  run_print_results("test_partition", test_partition);
+}
